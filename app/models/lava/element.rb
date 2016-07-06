4bde0e6ef0ae11f16dbf06fe474e7ead67483907
davidtengeri <david.tengeri@p92.hu>
2016-07-06 16:06:40 +0200

    Change video to use vimeo

--- app/models/lava/element.rb
+++ app/models/lava/element.rb
@@ -1,9 +1,18 @@
 module Lava
   class Element < ActiveRecord::Base
     include Models::Element
-    
+
+    before_save :convert_video_url
+
     belongs_to :user
     belongs_to :page
-    
+
+    private
+
+    def convert_video_url
+      if self.element_type == 'video' && self.value.match(/https:\/\/vimeo\.com\/.*/)
+        self.value = self.value.gsub(/https:\/\/vimeo\.com\//, 'https://player.vimeo.com/video/')
+      end
+    end
   end
 end
